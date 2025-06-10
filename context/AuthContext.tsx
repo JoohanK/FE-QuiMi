412c81946d97c64c21ec59313756e504b6044053
JoohanK <94johkri1@skola.boras.se>
2025-06-10 14:09:56 +0200

    fix

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,4 +1,3 @@
-// AuthContext.tsx
 import React, { createContext, useState, useEffect, ReactNode } from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "../firebaseConfig";
